3e40341705f6ad7262280968b2bd5b152732c6ce
perplexus <perplexus@ya.ru>
2018-09-12 11:42:53 +0300

    make clean

--- .rdma_init.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/rdma/rdma_module/rdma_init.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /root/rdma/rdma_module/rdma_init.ko /root/rdma/rdma_module/rdma_init.o /root/rdma/rdma_module/rdma_init.mod.o
