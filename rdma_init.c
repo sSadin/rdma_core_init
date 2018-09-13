58d9bd2bb802a9db13d0ba46f275e3b2e368f14c
Andrew Slinkin <a.slinkin@baum-inform.ru>
2018-09-13 13:11:56 +0300

    add ibv_open_device()

--- rdma_init.c
+++ rdma_init.c
@@ -5,37 +5,51 @@
 
 #define DRV KBUILD_MODNAME ": "
 
-struct ib_client nvdimm_client;
+struct ib_client ib_cache;
+
+struct ibv_context  *context;
 
 #define MAX_DEV 2
 /*???*/struct ib_device *m_devs[MAX_DEV];
 int devices;
 
-void add_device(struct ib_device* dev)
+void add_device( struct ib_device* dev )
 {
     int i;
-    m_devs[devices++] = dev;
-    if(devices > MAX_DEV)
+    m_devs[ devices++ ] = dev;
+    if( devices > MAX_DEV )
         --devices;
-    for(i=0; i<devices; ++i)
-        printk(DRV "We got a new device! The devece name is: %s\n",dev->name);
+    for( i = 0; i < devices; ++i )
+        printk( DRV "We got a new device! The devece name is: %s\n", m_devs[i]->name );
+
+    context = ibv_open_device(dev);
+    if( context == 0 )
+    {
+        printk( DRV "Can't open device: \n", dev->name );
+    }
 }
 
 
-void remove_device(struct ib_device* dev, void *ctx)
+void remove_device( struct ib_device* dev, void *ctx )
 {
-    printk(DRV "remove_device\n");
+    printk( DRV "remove_device\n" );
 }
 
 
-static int __init client_module_init(void)
+static int __init client_module_init( void )
 {
+    int ret;
     devices = 0;
-    nvdimm_client.name      = "DISAG_MEM";
-    nvdimm_client.add       = add_device;
-    nvdimm_client.remove    = remove_device;
+    ib_cache.name      = "DISAG_MEM";
+    ib_cache.add       = add_device;
+    ib_cache.remove    = remove_device;
 
-    ib_register_client(&nvdimm_client);
+    ret = ib_register_client( &ib_cache );
+    if (ret)
+    {
+        printk( KERN_ERR "Failed to register IB client\n" );
+        return ret;
+    }
 
 //    while(1);
     return 0;
@@ -45,10 +59,10 @@ static void __exit client_module_exit( void )
 {
 //    sock_release(sock);
 //    ib_unregister_event_handler(&ieh);
-    ib_unregister_client(&nvdimm_client);
-    printk(DRV "Exit client module.\n");
+    ib_unregister_client( &ib_cache );
+    printk( DRV "Exit client module.\n" );
 }
 
 module_init( client_module_init );
 module_exit( client_module_exit );
-MODULE_LICENSE("GPL");
+MODULE_LICENSE( "GPL" );
